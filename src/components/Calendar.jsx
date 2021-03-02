74e0158dd4bef7308580427dcb6ec4d8d9ffb151
Chris_Cunningham <this.cunningham@gmail.com>
2021-03-02 17:10:47 -0500

    Add calendar css module

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import css from '../styles/calendar.module.css';
 
 const Calendar = ({ blackouts }) => (
   <div className={css['calendar-container']}>
     Calendar Here!
-    <DayPicker />
+    <DayPicker disabledDays={blackouts} />
   </div>
 );
 
