12cc83f7e4506b99e178f25ce5f7051de71f7581
Chris_Cunningham <this.cunningham@gmail.com>
2021-03-02 17:12:24 -0500

    Add comment to calendar component

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,6 +3,7 @@ import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import css from '../styles/calendar.module.css';
 
+// plan is to pass in an array of blackout dates
 const Calendar = ({ blackouts }) => (
   <div className={css['calendar-container']}>
     Calendar Here!
