b34bfdb1cfaaa8e445b2f8daa9c7959173d95ccc
Chris_Cunningham <this.cunningham@gmail.com>
2021-03-09 19:07:23 -0500

    Images component refactored to react hooks

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,107 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import css from '../styles/images.module.css';
 
-export default class Images extends React.Component {
-  constructor(props) {
-    super(props);
+const Images = ({ images, id, travelersChoice }) => {
+  const [attractionImages, setAttractionImages] = useState([]);
+  const [displayedImage, setDisplayedImage] = useState('');
+  const [index, setIndex] = useState(0);
+  const [showAwardToolTip, setShowAwardToolTip] = useState(false);
 
-    this.state = {
-      attractionImages: [],
-      displayedImage: '',
-      index: 0,
-      showAwardToolTip: false,
-    };
-    this.changeImage = this.changeImage.bind(this);
-    this.toggleTooltip = this.toggleTooltip.bind(this);
-  }
-
-  componentDidMount() {
-    const { images } = this.props;
+  useEffect(() => {
     const sliced = images.slice();
-    this.setState({
-      attractionImages: sliced,
-      displayedImage: sliced[0],
-    });
-  }
+    setAttractionImages(sliced);
+    setDisplayedImage(sliced[0]);
+  }, []);
 
-  componentDidUpdate(prevProps) {
-    if (this.props !== prevProps) {
-      const { index } = this.state;
-      const { images, id } = this.props;
-      const sliced = images.slice();
+  useEffect(() => {
+    const sliced = images.slice();
+    setIndex(0);
+    setDisplayedImage(sliced[0]);
+  }, [id]);
 
-      if (id !== prevProps.id) {
-        this.setState({
-          index: 0,
-          displayedImage: sliced[0],
-        });
-      }
-      this.setState({
-        attractionImages: sliced,
-        displayedImage: sliced[index],
-      });
-    }
-  }
+  useEffect(() => {
+    const sliced = images.slice();
+    setAttractionImages(sliced);
+    setDisplayedImage(sliced[index]);
+  }, [index]);
 
-  changeImage(direction) {
-    const { index, attractionImages } = this.state;
+  const changeImage = (direction) => {
     if (direction === 'next') {
       if (attractionImages[index + 1] === undefined) {
         return;
       }
-      this.setState({
-        displayedImage: attractionImages[index + 1],
-        index: index + 1,
-      });
+      setDisplayedImage(attractionImages[index + 1]);
+      setIndex((i) => i + 1);
     }
     if (direction === 'prev') {
       if (attractionImages[index - 1] === undefined) {
         return;
       }
-      this.setState({
-        displayedImage: attractionImages[index - 1],
-        index: index - 1,
-      });
+      setDisplayedImage(attractionImages[index - 1]);
+      setIndex((i) => i - 1);
     }
-  }
+  };
 
-  toggleTooltip() {
-    const { showAwardToolTip } = this.state;
-    this.setState({
-      showAwardToolTip: !showAwardToolTip,
-    });
-  }
+  const toggleTooltip = () => {
+    setShowAwardToolTip((t) => !t);
+  };
 
-  render() {
-    const { travelersChoice, images } = this.props;
-    const { displayedImage, showAwardToolTip } = this.state;
-    return (
-      <div className={css.images}>
-        {travelersChoice && (
-          <img className={css.award} src="http://www.tripadvisor.com/img/cdsi/img2/awards/TC_2019_downloadable_L_R-40840-5.jpg" alt="Trip advisor award logo" onMouseEnter={this.toggleTooltip} onMouseLeave={this.toggleTooltip} />
-        )}
-        {showAwardToolTip && (
-          <div className={css.tooltip}>
-            <h3>What is Travelers’ Choice?</h3>
-            <p>
-              Tripadvisor gives a Travelers’ Choice award to accommodations, attractions and
-              restaurants that consistently earn great reviews from travelers and are ranked
-              within the top 10% of properties on Tripadvisor.
-            </p>
-          </div>
-        )}
-        {images.length > 1 ? (
-          <div className={css.overlay}>
-            <button type="button" className={css.next} onClick={() => this.changeImage('next')}>{'>'}</button>
-            <button type="button" className={css.prev} onClick={() => this.changeImage('prev')}>{'<'}</button>
-          </div>
-        ) : null}
-        <img src={displayedImage} alt="" className={css.image} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.images}>
+      {travelersChoice && (
+        <img className={css.award} src="http://www.tripadvisor.com/img/cdsi/img2/awards/TC_2019_downloadable_L_R-40840-5.jpg" alt="Trip advisor award logo" onMouseEnter={toggleTooltip} onMouseLeave={toggleTooltip} />
+      )}
+      {showAwardToolTip && (
+        <div className={css.tooltip}>
+          <h3>What is Travelers’ Choice?</h3>
+          <p>
+            Tripadvisor gives a Travelers’ Choice award to accommodations, attractions and
+            restaurants that consistently earn great reviews from travelers and are ranked
+            within the top 10% of properties on Tripadvisor.
+          </p>
+        </div>
+      )}
+      {images.length > 1 ? (
+        <div className={css.overlay}>
+          <button type="button" className={css.next} onClick={() => changeImage('next')}>{'>'}</button>
+          <button type="button" className={css.prev} onClick={() => changeImage('prev')}>{'<'}</button>
+        </div>
+      ) : null}
+      <img src={displayedImage} alt="" className={css.image} />
+    </div>
+  );
+};
+
+export default Images;
 
 Images.propTypes = {
   images: PropTypes.arrayOf(PropTypes.string).isRequired,
