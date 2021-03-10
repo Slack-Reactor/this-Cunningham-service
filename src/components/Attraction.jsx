43dfbfa0c22d19fe31196605c48957ddbbb03fa0
Chris_Cunningham <this.cunningham@gmail.com>
2021-03-10 11:16:59 -0500

    Refactor most of attraction component to hooks

--- src/components/Attraction.jsx
+++ src/components/Attraction.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './Header';
 import Overview from './Overview';
@@ -10,6 +10,150 @@ import Images from './Images';
 import css from '../styles/attraction.module.css';
 // const awsDNS = 'http://ec2-3-139-68-84.us-east-2.compute.amazonaws.com';
 const tripLogo = 'https://fec-tripadvisor-images.s3.us-east-2.amazonaws.com/images/Tripadvisor_Logo_circle-green_horizontal-lockup_registered-small_RGB.svg';
+
+const useForm = (initialValue) => {
+  const [clickImproved, setClickImproved] = useState(false);
+  const [form, setForm] = useState(initialValue);
+
+  const handleFormChange = (e) => {
+    let newValue = e.target.value;
+    if (e.target.name === 'suggestedDuration') {
+      newValue = Number(newValue);
+    }
+    if (newValue === 'true') {
+      newValue = true;
+    }
+    if (newValue === 'false') {
+      newValue = false;
+    }
+    setForm({
+      ...form,
+      [e.target.name]: newValue,
+    });
+  };
+
+  const openCloseForm = (current) => {
+    const {
+      description, address, isOpen, suggestedDuration,
+    } = current.overview;
+    setClickImproved((c) => !c);
+    setForm({
+      ...form,
+      description,
+      address,
+      isOpen,
+      suggestedDuration,
+    });
+  };
+
+  const submitImprovements = (id, e, current) => {
+    e.preventDefault();
+    if (JSON.stringify(form) === JSON.stringify(current.overview)) {
+      console.log('Must Submit Improvements to Current Attraction Listing');
+    } else {
+      axios.post(`/api/showcase/${id}`, { form })
+        .then(({ data }) => {
+          openCloseForm(current);
+          console.log(data.message);
+        })
+        .catch((err) => console.log('error', err));
+    }
+  };
+
+  return {
+    form,
+    clickImproved,
+    handleFormChange,
+    openCloseForm,
+    submitImprovements,
+  };
+};
+const initialFormState = {
+  description: '',
+  isOpen: false,
+  suggestedDuration: 0,
+  address: '',
+};
+
+const AttractionHooks = () => {
+  const [allAttractions, setAllAttractions] = useState([]);
+  const [current, setCurrent] = useState(null);
+  const [likeHover, setLikeHover] = useState(false);
+  const [browse, setBrowse] = useState(false);
+
+  const {
+    form,
+    clickImproved,
+    handleFormChange,
+    openCloseForm,
+    submitImprovements
+  } = useForm(initialFormState);
+
+  useEffect(() => {
+    axios.get('/api/showcase')
+      .then(({ data }) => {
+        setAllAttractions(data);
+        setCurrent(data[0]);
+      }).catch((err) => console.log('error GETTING all', err));
+  }, []);
+
+  const updateHeartHover = () => setLikeHover((h) => !h);
+  const updateLikeStatus = () => {
+    setCurrent({
+      ...current,
+      likedStatus: !current.likedStatus,
+    });
+  };
+  useEffect(() => {
+    axios.patch(`api/showcase/like/${current.id}`, { likedStatus: !current.likedStatus })
+      .catch((err) => {
+        console.log('Error PATCH likedStatus ', err);
+      });
+  }, [current.likedStatus]);
+
+  return (
+    <>
+      {current ? (
+        <div className={css.attraction}>
+          <div className={css.trip} onClick={() => setBrowse((b) => !b)}>
+            <img src={tripLogo} alt="triplogo" />
+          </div>
+          {browse && (
+          <div className={css.buttons}>
+            {allAttractions.map((attraction, i) => (
+              <button key={Math.random().toString()} className={css.browseButton} type="button" onClick={() => setCurrent(attraction)}>{i}</button>
+            ))}
+          </div>
+          )}
+          <Header
+            current={current}
+            updateHeartHover={updateHeartHover}
+            updateLikeStatus={updateLikeStatus}
+            likeHover={likeHover}
+          />
+          <Overview
+            overview={current.overview}
+            form={form}
+            clicked={clickImproved}
+            openCloseForm={this.openCloseForm}
+            handleFormChange={this.handleFormChange}
+            submitImprovements={this.submitImprovements}
+            id={current._id} /* eslint-disable-line no-underscore-dangle */
+          />
+          <Tickets
+            current={current}
+            blackouts={current.closedDays}
+          />
+          <Images
+            images={current.imageUrl}
+            travelersChoice={current.travelersChoiceAward}
+            id={current._id} /* eslint-disable-line no-underscore-dangle */
+          />
+        </div>
+      ) : <div className={css.loading}>Loading...new1</div>}
+    </>
+  )
+};
 
 export default class Attraction extends React.Component {
   constructor(props) {
