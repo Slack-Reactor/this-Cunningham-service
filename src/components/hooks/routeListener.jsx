b5e7fdb040e343a2224a7aa8dffb8eb216168f27
Chris_Cunningham <this.cunningham@gmail.com>
2021-03-13 12:03:40 -0500

    Add handleURLevent and handleIDclick to custom react hook

--- src/components/hooks/routeListener.jsx
+++ src/components/hooks/routeListener.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const initialState = {
-  id: 0,
-};
-
-const useRouteListener = (service) => {
+const useRouteListener = (initialState) => {
   const [masterState, setMasterState] = useState(initialState);
 
-  if (service === 'showcase') {
-    axios.get()
-  }
+  const handleIdClick = (id) => {
+    window.history.pushState({}, '', `http://localhost:3001/id/${id}`);
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  };
+
+  const handleUrlEvent = (service) => {
+    const id = window.location.pathname.slice(4);
+    if (service === 'showcase') {
+      axios.get(`http://localhost:3001/api/showcase/${id}`)
+        .then((data) => {
+          setMasterState(data);
+        })
+        .catch((err) => console.log('Error GET by id:', err));
+    }
+  };
 
   return {
     masterState,
-
+    handleIdClick,
+    handleUrlEvent,
   };
 };
 
